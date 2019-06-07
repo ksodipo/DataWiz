75d812ffc8b4645c67e7706e1f25648fe3efaa1e
Koye Sodipo <koye.sodipo@gmail.com>
2019-06-07 03:52:38 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'DataWiz',         
+  packages = ['DataWiz'],   
+  version = '0.3',      
+  #license='BSD-3-Clause',        
+  description = 'DataWiz helps new data learners, hobbyist and industry practitioners write Machine Learning code faster by doing the heavy-lifting such as data cleaning, data transformation, preparation and more! ',   # Give a short description about your library
+  author = 'Koye Sodipo',                   
+  author_email = 'koye.sodipo@gmail.com',      
+  url = 'https://github.com/ksodipo/DataWiz',   
+  download_url = 'https://github.com/ksodipo/DataWiz/archive/v0.3-beta.tar.gz',    
+  keywords = ['Data Engineering', 'data preparation', 'data science', 'data cleaning'],   
+  install_requires=[            
+          'numpy',
+          'pandas',
+          'sklearn',
+          'scipy'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT',
+    'Programming Language :: Python :: 2.7',  
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
